dd545ce649041b22646cffaf7a30a4e15c2146bc
kc <kcchawla2016@gmail.com>
2024-03-27 15:41:01 +0530

    second commit

--- src/test/java/pkg/SetupAndTearDown.java
+++ src/test/java/pkg/SetupAndTearDown.java
@@ -16,6 +16,6 @@ public class SetupAndTearDown {
 	public void stop() throws IOException, InterruptedException {
 		Runtime.getRuntime().exec("cmd /c start stop_dockergrid.bat");
 		Thread.sleep(30000);
-		Runtime.getRuntime().exec("taskkill /f /im cmd.exe");
+//		Runtime.getRuntime().exec("taskkill /f /im cmd.exe");
 	}
 }
